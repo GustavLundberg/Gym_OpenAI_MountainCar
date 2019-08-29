ca48d21a1f39778d4862323caf8ac0f7e71e98ee
Lundberg <gustav.k.lundberg@accenture.com>
2019-08-29 13:43:47 +0200

    Further improvements

--- MountainCar.py
+++ MountainCar.py
@@ -21,13 +21,13 @@ num_inputs = 2
 ##########################################
 
 num_episodes = 1
-num_individuals = 5
+num_individuals = 15
 dimensions = [num_actions] # For actions > 2, use num_actions number of output neurons, as well as predict_multiclass below
 # dimensions = [2, 1] # For num_actions = 2, use 1 neuron in the output layer, as well as predict_binary below
 activation_hidden = 'relu'
 activation_output = 'softmax'
-num_timesteps = 200
-num_generations = 12
+num_timesteps = 100 # 200 seems to be maximum
+num_generations = 7
 num_mutations = 2
 
 pos_weight = 1
@@ -57,15 +57,14 @@ for gen_num in range(num_generations):
 		fitness = 0
 		best_position = -2 # Best position during the entire episode
 		max_speed = 0 # Note that this is the magnitude if the velocity, i.e. direction does not matter
+		#worst_pos = 2
 		# Loop over the number of time steps
 		for t in range(num_timesteps):
 			env.render()
 			observation = np.reshape(observation, [1, num_inputs])
-			observation[0, 0] = observation[0, 0] + 0.3 # Attempt at 'normalizing' the inputs
+			observation[0, 0] = observation[0, 0] + 0.53 # Attempt at 'normalizing' the inputs (Want to make 0 at the point where the slope is zero, seems like 0.53 does the trick)
 			observation[0, 1] = 100 * observation[0, 1] # Attempt at 'normalizing' the inputs
-			#print('observation = ', observation, type(observation), observation.shape, observation[0,0], observation[0,1])
-
-			#print('Observation : ', type(observation), observation, observation.shape)
+			#print(observation)
 			
 			# For multiclass classification, choose predict_multiclass
 			# For binary classification, choose predict_binary
@@ -78,15 +77,29 @@ for gen_num in range(num_generations):
 			if observation[0] > best_position:
 				best_position = observation[0]
 
-			if abs(observation[1]) > max_speed:
-				max_speed = observation[1]
+			#if observation[0] < worst_pos:
+			#	worst_pos = observation[0]
 
-			# fitness += reward
-			#time.sleep(0.1)
+			if abs(observation[1]) > max_speed:
+				max_speed = abs(observation[1])
+
+			#print('max_speed = ', max_speed)
+
+			# The task is completed before time runs out
 			if done:
+				timesteps_before_completion = t
+				#print('timesteps_before_completion = ', timesteps_before_completion)
 				break
 
+		#print('best_position = ', best_position)
+		#print('worst_pos = ', worst_pos)
+
 		fitness = pos_weight * best_position + speed_weight * max_speed # Using best_position and max_speed as fitness instead of using the reward of the environment
+		
+		# Attempt at promoting individuals that completes the task. Gives an additional fitness based on the time it took to complete the task
+		if done:
+			fitness = fitness * (1 + timesteps_before_completion / num_timesteps)
+
 		print('Fitness for this episode = {}'.format(fitness))
 		individual.set_fitness(fitness)
 
